6ce2c85452e597133be46db06c5d7aa1c0f6582c
angus <446070848@qq.com>
2017-09-06 19:58:50 +0800

    ADD ECONOMIC SYS

---
+++ demo/game.lua
@@ -0,0 +1,1 @@
+game = g_class()
---
+++ demo/main.lua
@@ -0,0 +1,15 @@
+
+function love.load()
+    g_class = require "class"
+    g_imui = require "imui"
+    g_game = require "game"
+end
+
+function love.update(dt)
+    
+end
+
+function love.draw()
+
+end
+
